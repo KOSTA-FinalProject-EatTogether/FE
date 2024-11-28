b150f07fb9ffd18b68924c17326dc941b75b8b23
tjrqh <asdww6631@gmail.com>
2024-11-28 17:50:00 +0900

    fix : git ignore 수정

--- eat-together-fe/src/App.js
+++ eat-together-fe/src/App.js
@@ -1,52 +1,26 @@
-<<<<<<< HEAD
-import {RouterProvider} from "react-router-dom";
-import './App.css';
-import root from "./router/root";
 
-function App() {
-  return <RouterProvider router={root}/>
-=======
-import React from 'react';
-import Header from './Header';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-import React from 'react';
-import Header from './Header';
-import './App.css';
->>>>>>> Stashed changes
-=======
-import React from 'react';
-import Header from './Header';
-import './App.css';
->>>>>>> Stashed changes
-=======
-import React from 'react';
-import Header from './Header';
-import './App.css';
->>>>>>> Stashed changes
-import Restaurant from './components/restaurantDetails/RestaurantfixData';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <Header />
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-      <Restaurant />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
->>>>>>> f2ef88815d18ef98000c46193cbdca1a045871f8
 }
 
 export default App;
