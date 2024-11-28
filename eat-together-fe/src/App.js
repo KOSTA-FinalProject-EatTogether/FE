d44ef30b997a04b6654a3ba9d0bc820df1e14223
tjrqh <asdww6631@gmail.com>
2024-11-28 16:18:04 +0900

    feat : header, new Repository

--- eat-together-fe/src/App.js
+++ eat-together-fe/src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import Header from './Header';
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+import React from 'react';
+import Header from './Header';
+import './App.css';
+>>>>>>> Stashed changes
+=======
+import React from 'react';
+import Header from './Header';
+import './App.css';
+>>>>>>> Stashed changes
 import Restaurant from './components/restaurantDetails/RestaurantfixData';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <Header />
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
       <Restaurant />
     </div>
   );
