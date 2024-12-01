32be37267b338a28c18e8e4a385c65310d51f5f5
tjrqh <asdww6631@gmail.com>
2024-12-02 00:12:11 +0900

    #1 feat : 편의시설 기능 구현

---
+++ eat-together-fe/src/components/restaurantDetails/RestaurantDetailsNav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/restaurantDetailsCss/RestaunrantDetailsNav.css';
+
+function RestaurantDetailsNav() {
+  return (
+      /*
+      <div className="container">
+          <nav className="navbar">
+          <ul className="nav-menu">
+              <li className="nav-item"><Link to="/">홈</Link></li>
+              <li className="nav-item"><Link to="/menu">메뉴</Link></li>
+              <li className="nav-item"><Link to="/photos">사진</Link></li>
+              <li className="nav-item"><Link to="/reviews">리뷰</Link></li>
+              <li className="nav-item"><Link to="/store-details">매장상세</Link></li>
+          </ul>
+      </nav>
+      */
+      <div className="container">
+          <nav className="navbar">
+              <ul className="nav-menu">
+                  <li className="nav-item">홈</li>
+                  <li className="nav-item">메뉴</li>
+                  <li className="nav-item">사진</li>
+                  <li className="nav-item">리뷰</li>
+                  <li className="nav-item">매장상세</li>
+              </ul>
+          </nav>
+      </div>
+  );
+}
+
+export default RestaurantDetailsNav;
