81b8d0c4ff83fe3ddcf1f334be030d1425f4c3e2
tjrqh <asdww6631@gmail.com>
2024-12-02 04:55:31 +0900

    #1 feat : 사진 미리보기 구현

--- eat-together-fe/src/components/restaurantDetails/RestaurantDetailsPhoto.js
+++ eat-together-fe/src/components/restaurantDetails/RestaurantDetailsPhoto.js
@@ -6,37 +6,40 @@ const MenuItem = () => {
     return (
         <div className="restaurant_details_container">
             <div className="restaurant_details_title"><h2>사진</h2></div>
-                <div className="restaurant_details_wphoto_layout"> {/* 첫 번째 행 */}
-                    <div className="restaurant_details_photo_row">
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
+            <div className="restaurant_details_wphoto_layout"> {/* 첫 번째 행 */}
+                <div className="restaurant_details_photo_row">
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
                     </div>
-                    {/* 두 번째 행 */}
-                    <div className="restaurant_details_photo_row">
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
-                        <div className="restaurant_details_photo_container"><img src={simba}
-                                                                                 alt="Seafood Tower"
-                                                                                 className="restaurant_details_photo_image"/>
-                        </div>
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
+                    </div>
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
                     </div>
                 </div>
+                {/* 두 번째 행 */}
+                <div className="restaurant_details_photo_row">
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
+                    </div>
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
+                    </div>
+                    <div className="restaurant_details_photo_container"><img src={simba}
+                                                                             alt="Seafood Tower"
+                                                                             className="restaurant_details_photo_image"/>
+                    </div>
+                </div>
+            </div>
+            <div className="restaurant_details_button_container">
+                <button className="restaurant_details_button"> 사진 796개 전체보기 </button>
+            </div>
         </div>
     );
 };
