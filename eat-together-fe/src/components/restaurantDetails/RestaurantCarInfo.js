02ac0af3a818e578ccb87994411605fbdb6f0145
tjrqh <asdww6631@gmail.com>
2024-12-02 01:46:11 +0900

    #1 feat : 주차시설 레이아웃  구현

---
+++ eat-together-fe/src/components/restaurantDetails/RestaurantCarInfo.js
@@ -0,0 +1,12 @@
+
+
+const Restaurant_car_info = () => {
+
+
+    return(
+        <div className="restaurant_details_container">
+            <div className="restaurant_car_info_title"><h2>주차정보</h2></div>
+            <div className="restaurant_car_info_content">qweasdzxc</div>
+        </div>
+    )
+}
