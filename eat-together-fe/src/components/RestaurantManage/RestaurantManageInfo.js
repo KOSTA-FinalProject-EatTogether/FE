79fa3edbd744a82f7f220fa7952fb01f8a6be62d
tjrqh <asdww6631@gmail.com>
2024-12-02 06:59:52 +0900

    #1 feat : 메뉴 수정  구현

--- eat-together-fe/src/components/RestaurantManage/RestaurantManageInfo.js
+++ eat-together-fe/src/components/RestaurantManage/RestaurantManageInfo.js
@@ -1,18 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../../css/RestaurantManageCss/RestaurantManageInfo.css';
 
 const RestaurantManageInfo = () => {
-    const [restaurant_title, setTitle] = useState('강성별곡');
-    const [restaurant_text, setText] = useState('맛있음 ㅇㅇ');
-    const [restaurant_time, setTime] = useState('내 마음대로 영업함');
-    const [restaurant_location, setLocation] = useState('서울특별시 성동구 뚝섬로9길 8 3층');
-    const handleChange = (e) => {
-        setTitle(e.target.value);
-        setText(e.target.value);
-        setTime(e.target.value);
-        setLocation(e.target.value);
-
-    };
+    const [restaurantTitle, setTitle] = useState('강성별곡');
+    const [restaurantText, setText] = useState('맛있음 ㅇㅇ');
+    const [restaurantTime, setTime] = useState('내 마음대로 영업함');
+    const [restaurantLocation, setLocation] = useState('서울특별시 성동구 뚝섬로9길 8 3층');
 
     return (
         <div className="manage_container">
@@ -21,23 +14,23 @@ const RestaurantManageInfo = () => {
                 <div className="section">
                     <textarea
                         className="title_text_box"
-                        value={restaurant_title}
-                        onChange={handleChange}
+                        value={restaurantTitle}
+                        onChange={(e) => setTitle(e.target.value)}
                     ></textarea>
                     <textarea
                         className="text_box"
-                        value={restaurant_text}
-                        onChange={handleChange}
+                        value={restaurantText}
+                        onChange={(e) => setText(e.target.value)}
                     ></textarea>
                     <textarea
                         className="text_box"
-                        value={restaurant_time}
-                        onChange={handleChange}
+                        value={restaurantTime}
+                        onChange={(e) => setTime(e.target.value)}
                     ></textarea>
                     <textarea
                         className="text_box"
-                        value={restaurant_location}
-                        onChange={handleChange}
+                        value={restaurantLocation}
+                        onChange={(e) => setLocation(e.target.value)}
                     ></textarea>
                     <button className="edit_button">수정</button>
                 </div>
